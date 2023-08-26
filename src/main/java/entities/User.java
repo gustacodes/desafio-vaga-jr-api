e4c42ff0542c4353451b3cbe7bd9e10c8f1bb128
Gustavo Alencar <gustavorique@live.com>
2023-08-26 01:32:47 -0300

    feat: create class entitie user

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,20 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String cpf;
+    private String phone;
+    private String learning;
+
+}
